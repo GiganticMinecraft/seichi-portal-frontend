19bcc96170d3624ebcf4b8f6fca155024c8c2d64
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-11-19 23:02:44 +0900

    feat: hide header when error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,27 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import { ChakraProvider } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+
+import { MainLayout } from '@/components/Layout';
 
 import type { AppProps } from 'next/app';
 
-const App = ({ Component, pageProps }: AppProps) => (
-  <ChakraProvider>
-    <Component {...pageProps} />
-  </ChakraProvider>
-);
+const App = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
+  if (router.pathname === '/_error')
+    return (
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    );
+
+  return (
+    <ChakraProvider>
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
+    </ChakraProvider>
+  );
+};
 
 export default App;
