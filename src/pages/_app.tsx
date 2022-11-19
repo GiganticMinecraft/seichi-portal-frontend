5d72b308da1c24aea9dc0c5a1601cd04f9db2207
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-11-19 23:22:11 +0900

    feat: make default font size larger

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,32 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
 
 import { MainLayout } from '@/components/Layout';
 
 import type { AppProps } from 'next/app';
 
+const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        fontSize: 'xl',
+      },
+    },
+  },
+});
+
 const App = ({ Component, pageProps }: AppProps) => {
   const router = useRouter();
   if (router.pathname === '/_error')
     return (
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <Component {...pageProps} />
       </ChakraProvider>
     );
 
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <MainLayout>
         <Component {...pageProps} />
       </MainLayout>
