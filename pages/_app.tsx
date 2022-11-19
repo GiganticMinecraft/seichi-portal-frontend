d65a0564740c0a674766c458d383be6924ec7cf1
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-11-19 15:16:40 +0900

    style: run lint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps) {
+const App = ({ Component, pageProps }: AppProps) => (
   // eslint-disable-next-line react/jsx-props-no-spreading
-  return <Component {...pageProps} />;
-}
+  <Component {...pageProps} />
+);
+
+export default App;
