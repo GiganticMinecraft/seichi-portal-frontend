30890fff84e10c682f81cc9a77345b2bc4eae9a4
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-11-20 11:40:29 +0900

    refactor: 別に2ファイルに分ける必要はなさそう

--- src/components/Helmet/presenter.tsx
+++ src/components/Helmet/presenter.tsx
@@ -1,13 +1,15 @@
 import Head from 'next/head';
 
 type Props = {
-  title: string;
-  description: string;
+  title?: string;
+  description?: string;
 };
 
-export const Presenter = ({ title, description }: Props) => (
+const siteName = 'SeichiPortal';
+
+export const Helmet = ({ title, description = '' }: Props) => (
   <Head>
-    <title>{title}</title>
+    <title>{title ? `${title} | ${siteName}` : siteName}</title>
     <meta name="description" content={description} />
   </Head>
 );
