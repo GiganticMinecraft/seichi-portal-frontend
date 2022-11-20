4c336aaa855f0261ddc3d841ccb4f1734083a534
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-11-20 11:44:50 +0900

    refactor: use ServerLogo

--- src/components/Header/presenter.tsx
+++ src/components/Header/presenter.tsx
@@ -1,17 +1,13 @@
 import { Box, Button } from '@chakra-ui/react';
-import Image from 'next/image';
 import Link from 'next/link';
+
+import { ServerLogo } from '../ServerLogo';
 
 export const Header = () => (
   <Box bg="cyan.300" py="3" textAlign="center">
     <Button variant="link">
       <Link href="/">
-        <Image
-          src="https://github.com/GiganticMinecraft/branding/blob/master/server-logo-black.png?raw=true"
-          alt="ギガンティック☆整地鯖のロゴ"
-          width={110}
-          height={100}
-        />
+        <ServerLogo width={110} height={100} color="dark" />
       </Link>
     </Button>
   </Box>
