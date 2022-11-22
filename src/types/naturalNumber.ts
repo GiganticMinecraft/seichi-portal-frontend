ceceeffac3d0f46d200bd9199b4c7166858ce2e7
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-11-22 16:08:53 +0900

    fix: NaturalNumber should depend on ZeroOrNaturalNumber

--- src/types/naturalNumber.ts
+++ src/types/naturalNumber.ts
@@ -1,12 +1,16 @@
-import { assertNumber, isNumber } from './assertNumber';
 import { Nominal } from './nominal';
+import {
+  assertZeroOrNaturalNumber,
+  isZeroOrNaturalNumber,
+} from './zeroOrNaturalNumber';
 
 export type NaturalNumber = Nominal<number, 'NaturalNumber'>;
 
-const isNaturalNumber = (v: unknown): v is number => isNumber(v) && v > 0;
+const isNaturalNumber = (v: unknown): v is number =>
+  isZeroOrNaturalNumber(v) && v !== 0;
 
 export function assertNaturalNumber(v: unknown): asserts v is NaturalNumber {
-  assertNumber(v);
+  assertZeroOrNaturalNumber(v);
 
   if (!isNaturalNumber(v)) throw new Error('The value must be greater than 0');
 }
