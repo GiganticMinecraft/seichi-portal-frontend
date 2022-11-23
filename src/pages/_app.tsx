74cbda77200dc5879170456698148fcc7446a820
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-11-23 22:54:54 +0900

    feat: run msw when dev

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,11 @@ import { MainLayout } from '@/components/Layout';
 
 import type { AppProps } from 'next/app';
 
+if (process.env.NODE_ENV === 'development') {
+  const MockServer = () => import('@/__mocks__/worker');
+  MockServer();
+}
+
 const App = ({ Component, pageProps }: AppProps) => {
   const router = useRouter();
   if (router.pathname === '/_error')
