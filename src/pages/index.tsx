84a832718ab13b7478696fc34bd9cee622f9646d
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-11-23 18:40:11 +0900

    feat: add a link to `/forms`

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
+import { ChakraLink } from '@/components/ChakraLink';
 import { ContentLayout } from '@/components/Layout';
+import { NextLink } from '@/components/NextLink';
 
 const Index = () => (
   <ContentLayout title="Home" description="Seichi Server Portal Site">
-    content!
+    <NextLink href="/forms">
+      <ChakraLink>フォーム一覧</ChakraLink>
+    </NextLink>
   </ContentLayout>
 );
 
