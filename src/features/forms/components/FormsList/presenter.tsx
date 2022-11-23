da92817d3e97156ecfa6922246c0de1a3571a76d
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-11-23 21:21:00 +0900

    feat: there is no available forms in FormList

--- src/features/forms/components/FormsList/presenter.tsx
+++ src/features/forms/components/FormsList/presenter.tsx
@@ -1,4 +1,13 @@
-import { Box, Heading, Stack, StackDivider, Text } from '@chakra-ui/react';
+import {
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  Box,
+  Heading,
+  Stack,
+  StackDivider,
+  Text,
+} from '@chakra-ui/react';
 
 import type { FormId, FormInfo } from '../../types';
 
@@ -9,13 +18,20 @@ type Props = {
 
 export const Presenter = ({ forms, onClick = () => undefined }: Props) => (
   <Stack spacing={4} divider={<StackDivider />}>
-    {forms.map((form) => (
-      <Box key={form.id} onClick={() => onClick(form.id)}>
-        <Heading fontSize="lg" mb={2}>
-          {form.title}
-        </Heading>
-        <Text fontSize="md">{form.description}</Text>
-      </Box>
-    ))}
+    {forms.length === 0 ? (
+      <Alert status="error">
+        <AlertIcon />
+        <AlertTitle>現在回答可能なフォームはありません</AlertTitle>
+      </Alert>
+    ) : (
+      forms.map((form) => (
+        <Box key={form.id} onClick={() => onClick(form.id)}>
+          <Heading fontSize="lg" mb={2}>
+            {form.title}
+          </Heading>
+          <Text fontSize="md">{form.description}</Text>
+        </Box>
+      ))
+    )}
   </Stack>
 );
