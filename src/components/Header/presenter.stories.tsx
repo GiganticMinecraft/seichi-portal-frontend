a7f39eb7cac67ae02429f0620249cb83928a4c62
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-11-23 21:43:00 +0900

    fix(storybook): fix directory names

--- src/components/Header/presenter.stories.tsx
+++ src/components/Header/presenter.stories.tsx
@@ -2,6 +2,9 @@ import { ComponentMeta, ComponentStoryObj } from '@storybook/react';
 
 import { Header } from './presenter';
 
-export default { component: Header } as ComponentMeta<typeof Header>;
+export default {
+  component: Header,
+  title: 'components/Header',
+} as ComponentMeta<typeof Header>;
 
 export const Index: ComponentStoryObj<typeof Header> = { args: {} };
--- src/components/ServerLogo/presenter.stories.tsx
+++ src/components/ServerLogo/presenter.stories.tsx
@@ -2,7 +2,10 @@ import { ComponentMeta, ComponentStoryObj } from '@storybook/react';
 
 import { ServerLogo } from './presenter';
 
-export default { component: ServerLogo } as ComponentMeta<typeof ServerLogo>;
+export default {
+  component: ServerLogo,
+  title: 'components/ServerLogo',
+} as ComponentMeta<typeof ServerLogo>;
 
 export const Dark: ComponentStoryObj<typeof ServerLogo> = {
   args: {
