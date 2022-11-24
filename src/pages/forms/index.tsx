dc9fc54a07f9341edd11091abfdd8b7303465903
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-11-24 17:25:42 +0900

    feat: add Fallback

--- src/pages/forms/index.tsx
+++ src/pages/forms/index.tsx
@@ -1,17 +1,17 @@
 import { Suspense } from 'react';
 
 import { ErrorBoundary } from '@/components/ErrorBoundary';
+import { Fallback } from '@/components/Fallback';
 import { ContentLayout } from '@/components/Layout';
 import { FormsList } from '@/features/forms/components/FormsList';
 
-// TODO: fallback
 const ShowFormsList = () => (
   <ContentLayout
     title="フォーム一覧"
     description="回答できるフォームの一覧を表示します。"
   >
     <ErrorBoundary>
-      <Suspense fallback="Loading...">
+      <Suspense fallback={<Fallback />}>
         <FormsList />
       </Suspense>
     </ErrorBoundary>
