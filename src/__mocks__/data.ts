90b5844e63e299921380c6fe21f8d08efc5417ad
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-11-24 17:51:53 +0900

    chore: fix formid in mock data

--- src/__mocks__/data.ts
+++ src/__mocks__/data.ts
@@ -3,12 +3,12 @@ import { asNonEmptyString } from '@/types';
 
 export const formInfoList: FormInfo[] = [
   {
-    id: asFormId(1),
+    id: asFormId(0),
     title: asNonEmptyString('通報'),
     description: '通報フォームです。',
   },
   {
-    id: asFormId(2),
+    id: asFormId(1),
     title: asNonEmptyString('不具合報告'),
     description: '不具合報告フォームです。',
   },
