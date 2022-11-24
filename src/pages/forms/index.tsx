788d2fbe583b53ebb853ee3c1c3baf90e7ab77ec
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-11-24 16:56:36 +0900

    feat: wrap FormsList with EB and Suspense

--- src/pages/forms/index.tsx
+++ src/pages/forms/index.tsx
@@ -1,12 +1,20 @@
+import { Suspense } from 'react';
+
+import { ErrorBoundary } from '@/components/ErrorBoundary';
 import { ContentLayout } from '@/components/Layout';
 import { FormsList } from '@/features/forms/components/FormsList';
 
+// TODO: fallback
 const ShowFormsList = () => (
   <ContentLayout
     title="フォーム一覧"
     description="回答できるフォームの一覧を表示します。"
   >
-    <FormsList />
+    <ErrorBoundary>
+      <Suspense fallback="Loading...">
+        <FormsList />
+      </Suspense>
+    </ErrorBoundary>
   </ContentLayout>
 );
 
