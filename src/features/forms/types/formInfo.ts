dbdc90066954219aff47aeb8b36f1c5b92eb5818
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-11-24 16:55:03 +0900

    feat: add type guards for FormInfo

--- src/features/forms/types/formInfo.ts
+++ src/features/forms/types/formInfo.ts
@@ -1,3 +1,21 @@
+import { isNonEmptyString } from '@/types';
+import { isString } from '@/types/assertString';
+
+import { isFormId } from './formId';
+
 import type { Form } from './form';
 
 export type FormInfo = Pick<Form, 'id' | 'title' | 'description'>;
+
+export const isFormInfo = (arg: unknown): arg is FormInfo => {
+  const i = arg as FormInfo;
+
+  return (
+    isFormId(i?.id) &&
+    isNonEmptyString(i?.description) &&
+    isString(i?.description)
+  );
+};
+
+export const isFormInfoList = (args: unknown[]): args is FormInfo[] =>
+  args.every((arg) => isFormInfo(arg));
