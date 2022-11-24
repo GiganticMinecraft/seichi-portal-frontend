45666d03efc7af9b8717d0a65155116efc25b450
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-11-24 16:44:58 +0900

    feat: add ErrorBoundary

---
+++ src/components/ErrorBoundary.tsx
@@ -0,0 +1,50 @@
+import { HTTPError } from 'ky';
+import { ErrorInfo, PureComponent, ReactNode } from 'react';
+
+import { ErrorAlert } from './Alert';
+
+type StatusMessages = { [status: number]: string };
+type Props = {
+  children: ReactNode;
+  statusMessages?: StatusMessages;
+  onError?: () => void;
+};
+type State = { hasError: boolean; error: Error | null };
+const DEFAULT_MESSAGES: StatusMessages = { 0: 'サーバエラーです' };
+
+export class ErrorBoundary extends PureComponent<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { hasError: false, error: null };
+  }
+
+  static getDerivedStateFromError = (error: Error): State => ({
+    hasError: true,
+    error,
+  });
+
+  componentDidCatch(error: Error, info: ErrorInfo) {
+    const { onError } = this.props;
+    if (onError) onError();
+
+    console.error(error, info); // eslint-disable-line no-console
+  }
+
+  render(): ReactNode {
+    const { children, statusMessages = {} } = this.props;
+    const { hasError, error } = this.state;
+    const messages = { ...DEFAULT_MESSAGES, ...statusMessages };
+
+    if (hasError) {
+      const statusCode = (error as HTTPError)?.response?.status;
+
+      if (statusCode && Object.keys(messages).includes(String(statusCode))) {
+        return <ErrorAlert title={messages[statusCode]} />;
+      }
+
+      return <ErrorAlert title={messages[0]} />;
+    }
+
+    return children;
+  }
+}
