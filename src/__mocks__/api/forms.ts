56b0878ed756638289b7a6cef938ca269e6680f5
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-11-24 17:47:05 +0900

    refactor: fix handler name

--- src/__mocks__/api/forms.ts
+++ src/__mocks__/api/forms.ts
@@ -2,7 +2,7 @@ import { MockedRequest, ResponseResolver, restContext } from 'msw';
 
 import { formInfoList } from '../data';
 
-export const mockListForms: ResponseResolver<
+export const mockListFormInfo: ResponseResolver<
   MockedRequest,
   typeof restContext
 > = async (_req, res, ctx) => res(ctx.status(200), ctx.json(formInfoList));
--- src/__mocks__/handler.ts
+++ src/__mocks__/handler.ts
@@ -2,6 +2,6 @@ import { rest } from 'msw';
 
 import { API_URL } from '@/config';
 
-import { mockListForms } from './api/forms';
+import { mockListFormInfo } from './api/forms';
 
-export const handlers = [rest.get(`${API_URL}/forms`, mockListForms)];
+export const handlers = [rest.get(`${API_URL}/forms`, mockListFormInfo)];
