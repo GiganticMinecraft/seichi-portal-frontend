bb76a7b95cc9e643b444cd5d91956be8c8c7f888
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-11-25 13:33:00 +0900

    chore: remove Next/Image hacks from sb config

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,3 @@
-import * as nextImage from 'next/image';
-
-Object.defineProperty(nextImage, 'default', {
-  configurable: true,
-  value: (props) => <img {...props} />,
-});
-
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
