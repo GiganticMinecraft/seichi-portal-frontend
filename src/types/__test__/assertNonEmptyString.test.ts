df3aab8c89abdb144da2d20ae5c5e046a73b9aef
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-11-25 13:24:07 +0900

    test: add test for isNonEmptyStringList

--- src/types/__test__/assertNonEmptyString.test.ts
+++ src/types/__test__/assertNonEmptyString.test.ts
@@ -1,4 +1,4 @@
-import { isNonEmptyString } from '../nonEmptyString';
+import { isNonEmptyString, isNonEmptyStringList } from '../nonEmptyString';
 
 describe('isNonEmptyString', () => {
   it.each(['How about dancing with me?', 'Good night, Sir'])(
@@ -10,6 +10,22 @@ describe('isNonEmptyString', () => {
 
   it('should return false when given empty string values', () => {
     expect(isNonEmptyString('')).toBeFalsy();
+  });
+});
+
+describe('isNonEmptyStringList', () => {
+  it('should return true when given non empty string values list', () => {
+    expect(
+      isNonEmptyStringList(['How about dancing with me?', 'Good night, Sir']),
+    ).toBeTruthy();
+  });
+
+  it('should return true when given empty list', () => {
+    expect(isNonEmptyStringList([])).toBeTruthy();
+  });
+
+  it('should return false when given empty string values list', () => {
+    expect(isNonEmptyStringList(['', '', ''])).toBeFalsy();
   });
 });
 
