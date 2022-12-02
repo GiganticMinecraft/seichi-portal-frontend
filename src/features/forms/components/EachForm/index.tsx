45ab04df075c8d9fc2b8727e7fad656c3dbee562
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-12-02 22:44:23 +0900

    feat: impl each form

--- src/features/forms/components/EachForm/index.tsx
+++ src/features/forms/components/EachForm/index.tsx
@@ -1,4 +1,5 @@
 import { useRouter } from 'next/router';
+import { useForm } from 'react-hook-form';
 
 import { useForm as fetchForm } from './hooks';
 import { Presenter } from './presenter';
@@ -6,9 +7,22 @@ import { Presenter } from './presenter';
 import { asFormId } from '../../types';
 
 export const EachForm = () => {
-  const { query, isReady } = useRouter<'/forms/[id]'>();
-  if (!isReady) return null;
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm();
+  const { query } = useRouter<'/forms/[id]'>();
   const form = fetchForm(asFormId(query.id));
+  if (!form) return null;
+  const onSubmit = handleSubmit(
+    (data) =>
+      new Promise((resolve) => {
+        // TODO: impl
+        alert(JSON.stringify(data, null, 2));
+        resolve(data);
+      }),
+  );
 
-  return form ? <Presenter {...{ form }} /> : null;
+  return <Presenter {...{ form, register, onSubmit, isSubmitting }} />;
 };
