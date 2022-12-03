9aad19ec79032941f7ee15b1f234d5020da6715c
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-12-04 00:58:06 +0900

    chore: fix image asset url prefix

--- export-images.config.js
+++ export-images.config.js
@@ -3,7 +3,6 @@ const urlPrefix = process.env.GITHUB_ACTIONS ? '/seichi-portal-frontend' : '';
 /** @type {import('next-export-optimize-images').Config} */
 const config = {
   basePath: urlPrefix,
-  imageDir: `${urlPrefix}/_next/static/chunks/images`,
 };
 
 module.exports = config;
