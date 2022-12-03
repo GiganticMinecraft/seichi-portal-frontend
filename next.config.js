c028cbcbe2fd1422b2e5fd5e9ccdf6875c9630aa
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-12-04 00:25:02 +0900

    chore: fix image asset url prefix

--- next.config.js
+++ next.config.js
@@ -1,13 +1,15 @@
 const path = require('path');
 const withExportImages = require('next-export-optimize-images');
 
+const urlPrefix = process.env.GITHUB_ACTIONS ? '/seichi-portal-frontend' : '';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   // https://jamband.github.io/blog/2021/08/deploy-nextjs-app-to-github-pages/
   trailingSlash: true,
-  basePath: process.env.GITHUB_ACTIONS && '/seichi-portal-frontend',
+  assetPrefix: urlPrefix,
+  basePath: urlPrefix,
   webpack(config) {
     config.resolve.alias['@'] = path.join(__dirname, 'src');
 
