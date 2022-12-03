96e2cc7960ea25b5e1d50b47a1c90ad092b9ec47
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-12-03 21:02:30 +0900

    chore: fix Next.js config for GHPages
    close GHPagesでリンクが壊れている #20

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const withExportImages = require('next-export-optimize-images');
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // https://jamband.github.io/blog/2021/08/deploy-nextjs-app-to-github-pages/
+  trailingSlash: true,
+  basePath: process.env.GITHUB_ACTIONS && '/seichi-portal-frontend',
   webpack(config) {
     config.resolve.alias['@'] = path.join(__dirname, 'src');
 
