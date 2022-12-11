750738743795711291e680be46775ad0c40a3ee0
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-12-11 17:41:53 +0900

    feat: add AnswerCompleted component

---
+++ src/features/forms/components/AnswerCompleted/presenter.tsx
@@ -0,0 +1,37 @@
+import { Button, Divider, Stack } from '@chakra-ui/react';
+import { FaArrowLeft, FaRedo } from 'react-icons/fa';
+
+import { Alert } from '@/components/Alert';
+
+type Props = {
+  onBackToFormsList: () => void;
+  onSendAgein: () => void;
+};
+
+export const AnswerCompleted = ({ onBackToFormsList, onSendAgein }: Props) => (
+  <>
+    <Alert
+      status="success"
+      title="回答を送信しました"
+      description="ご回答いただきありがとうございます。"
+    />
+    <Divider my={3} />
+    <Stack spacing={4} justifyContent="center" direction={['column', 'row']}>
+      <Button
+        colorScheme="blue"
+        leftIcon={<FaArrowLeft />}
+        onClick={onBackToFormsList}
+      >
+        フォーム一覧に戻る
+      </Button>
+      <Button
+        colorScheme="blue"
+        leftIcon={<FaRedo />}
+        variant="outline"
+        onClick={onSendAgein}
+      >
+        別の回答を送信する
+      </Button>
+    </Stack>
+  </>
+);
