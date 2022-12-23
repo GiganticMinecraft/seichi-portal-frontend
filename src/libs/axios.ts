10290f5cae701c20c1569b95fe8d081f437a1219
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-12-23 23:37:24 +0900

    chore: add TODO

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -2,6 +2,7 @@ import Axios from 'axios';
 
 export const axios = Axios.create({
   timeout: 7000,
+  // TODO: コメントアウトを外す
   // transformResponse: [
   //   (data) => JSON.stringify(camelcaseKeys(data, { deep: true }), null, 2),
   // ],
