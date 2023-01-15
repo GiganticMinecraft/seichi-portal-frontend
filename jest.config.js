1716335de46e762f6b7451548518bade7a9427ef
Lucky <Lucky3028@users.noreply.github.com>
2023-01-15 14:02:46 +0900

    chore: add Jest repoters setting

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,7 @@ const createJestConfig = nextJest({ dir: './' });
 const customJestConfig = {
   moduleNameMapper: { '^@/(.*)$': '<rootDir>/src/$1' },
   testEnvironment: 'jest-environment-jsdom',
+  reporters: ['default', 'github-actions'],
 };
 
 module.exports = createJestConfig(customJestConfig);
