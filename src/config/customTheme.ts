ad115ee7e6632742426ee7fb2edb26683e1d3981
Lucky3028 <Lucky3028@users.noreply.github.com>
2023-02-11 18:37:10 +0900

    refactor: add types for theme

--- src/config/customTheme.ts
+++ src/config/customTheme.ts
@@ -1,18 +1,30 @@
-import { extendTheme } from '@chakra-ui/react';
+import {
+  extendTheme,
+  type ThemeConfig,
+  type ComponentSingleStyleConfig,
+} from '@chakra-ui/react';
+
+const theme: ThemeConfig = {
+  initialColorMode: 'light',
+  useSystemColorMode: true,
+};
+
+const containerStyle: ComponentSingleStyleConfig = {
+  baseStyle: {
+    maxW: {
+      base: '60ch',
+      md: '120ch',
+    },
+  },
+};
 
 export const customChakraTheme = extendTheme({
+  ...theme,
   breakPoints: {
     xl: '1440px',
     '2xl': '2560px',
   },
   components: {
-    Container: {
-      baseStyle: {
-        maxW: {
-          base: '60ch',
-          md: '120ch',
-        },
-      },
-    },
+    Container: containerStyle,
   },
 });
