12627fc6d5762d3e429e1e0f5dfbdcbf9f9c3b94
Lucky3028 <Lucky3028@users.noreply.github.com>
2023-02-11 20:51:27 +0900

    fix: Image should have priority

--- src/components/ServerLogo/presenter.tsx
+++ src/components/ServerLogo/presenter.tsx
@@ -10,6 +10,7 @@ export const Presenter = ({ color, width, height }: ServerLogoProps) => (
   <Image
     src={`https://github.com/GiganticMinecraft/branding/blob/master/server-logo-${color}.png?raw=true`}
     alt="ギガンティック☆整地鯖のロゴ"
+    priority
     // https://ebisu.com/note/next-image-migration/
     style={{
       maxWidth: '100%',
