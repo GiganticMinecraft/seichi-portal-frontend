36486924769a4de2c7cca25f4188ebd3c6783200
Lucky3028 <Lucky3028@users.noreply.github.com>
2023-02-11 16:33:58 +0900

    refactor: use { type }

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { type AppProps } from 'next/app';
 
 import { customTheme } from '@/config/customTheme';
 
