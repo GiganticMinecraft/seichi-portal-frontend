bf17a7bd90aa13a3e295d40ccdace75313bb5fdf
Lucky3028 <Lucky3028@users.noreply.github.com>
2023-02-11 21:12:08 +0900

    chore: add testPathIgnorePatterns in jest config

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,10 @@ const nextJest = require('next/jest');
 
 const createJestConfig = nextJest({ dir: './' });
 
+/** @type {import('jest').Config} */
 const customJestConfig = {
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  // パスエイリアスの設定
   moduleNameMapper: { '^@/(.*)$': '<rootDir>/src/$1' },
   testEnvironment: 'jest-environment-jsdom',
 };
