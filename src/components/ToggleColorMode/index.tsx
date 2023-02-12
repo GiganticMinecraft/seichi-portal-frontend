eed48fd33d2ddc0843bc493a4de9a90495908338
Lucky3028 <Lucky3028@users.noreply.github.com>
2023-02-12 20:15:02 +0900

    refactor: rename object property

--- src/components/ToggleColorMode/index.tsx
+++ src/components/ToggleColorMode/index.tsx
@@ -3,13 +3,13 @@ import { useColorMode } from '@chakra-ui/react';
 import { Presenter, type ToggleColorModeProps } from './presenter';
 
 export const ToggleColorMode = (props: ToggleColorModeProps) => {
-  const { colorMode, toggleColorMode } = useColorMode();
+  const { colorMode: currentColorMode, toggleColorMode } = useColorMode();
 
   return (
     <Presenter
       // eslint-disable-next-line react/jsx-props-no-spreading
       {...props}
-      {...{ currentColorMode: colorMode, toggleColorMode }}
+      {...{ currentColorMode, toggleColorMode }}
     />
   );
 };
