fa9dff9059871657467cdbbebf7454082dd7e216
Lucky3028 <Lucky3028@users.noreply.github.com>
2023-02-12 23:02:51 +0900

    feat: Header should have ToggleColorMode

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,23 @@
 import { Box } from '@chakra-ui/react';
 
 import { ServerLogo } from '../ServerLogo';
+import { ToggleColorMode } from '../ToggleColorMode';
 
 export const Header = () => (
   <Box
     boxShadow="md"
-    py={3}
+    p={3}
     display="flex"
-    justifyContent="center"
+    justifyContent={{ base: 'space-between', md: 'center' }}
+    alignItems="center"
     position="relative"
   >
-    <ServerLogo width={120} height={100} />
+    <ServerLogo width={120} />
+    <Box
+      position={{ base: 'relative', md: 'absolute' }}
+      right={{ base: 0, md: 40 }}
+    >
+      <ToggleColorMode />
+    </Box>
   </Box>
 );
