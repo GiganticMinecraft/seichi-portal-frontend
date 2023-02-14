61fe43139ae07fe2f44cb309635fd0dd68a65988
Lucky3028 <Lucky3028@users.noreply.github.com>
2023-02-14 19:18:45 +0900

    test: add test for undefined

--- src/components/elements/Header/__test__/presenter.test.tsx
+++ src/components/elements/Header/__test__/presenter.test.tsx
@@ -6,10 +6,12 @@ describe('Header', () => {
   it('should contain SignOut button', () => {
     render(<Presenter isAuthenticated />);
     expect(screen.getByLabelText('サインアウトする')).toBeDefined();
+    expect(screen.queryByLabelText('サインインする')).toBeNull();
   });
 
   it('should contain SignIn button', () => {
     render(<Presenter isAuthenticated={false} />);
     expect(screen.getByLabelText('サインインする')).toBeDefined();
+    expect(screen.queryByLabelText('サインアウトする')).toBeNull();
   });
 });
