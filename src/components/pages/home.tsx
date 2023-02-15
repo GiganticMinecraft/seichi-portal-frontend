e9b6e2cc447616db7dc383d5f369023019c89a7b
Lucky3028 <Lucky3028@users.noreply.github.com>
2023-02-15 14:23:56 +0900

    feat: add HOme component

---
+++ src/components/pages/home.tsx
@@ -0,0 +1,7 @@
+import { Layout } from '@/components/elements/Layout';
+
+export const Home = () => (
+  <Layout title="Home" description="このページはトップ画面です。">
+    <p>Hello!</p>
+  </Layout>
+);
---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+import { Home } from '@/components/pages/home';
+
+const Component = () => <Home />;
+
+export default Component;
