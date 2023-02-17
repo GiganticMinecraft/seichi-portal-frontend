72a23504234887ac03320cb775928d24a945af03
Lucky3028 <Lucky3028@users.noreply.github.com>
2023-02-17 19:20:01 +0900

    fix: move ReactQueryDevtools

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,9 +14,9 @@ const App = ({ Component, pageProps }: AppProps) => (
     <ChakraProvider theme={customChakraTheme}>
       <MsalProvider>
         <Component {...pageProps} />
-        <ReactQueryDevtools initialIsOpen={false} />
       </MsalProvider>
     </ChakraProvider>
+    <ReactQueryDevtools initialIsOpen={false} />
   </QueryClientProvider>
 );
 
