e175b42c991a27afca356a3d7dfbaaa29f22c650
Lucky3028 <Lucky3028@users.noreply.github.com>
2023-02-17 15:59:46 +0900

    chore: replace multi line comment with single lines

--- src/features/user/api/index.ts
+++ src/features/user/api/index.ts
@@ -19,10 +19,8 @@ export const getMinecraftGameProfile = async ([
   const xstsToken = await requireXstsToken(xblToken);
   const mcAccessToken = await requireMcAccessToken(xstsToken);
   const hasMc = await hasMcAccount(mcAccessToken);
-  /**
-   * TODO: ここの条件分岐がfalseになる（MCアカウントをもっている）のに、getMcProfileが404になる場合がある
-   * これは、アカウントをもっているにも関わらず、アカウント名を設定していないため
-   */
+  // TODO: ここの条件分岐がfalseになる（MCアカウントをもっている）のに、getMcProfileが404になる場合がある
+  // これは、アカウントをもっているにも関わらず、アカウント名を設定していないため
   if (!hasMc) {
     throw new Error("The Microsoft account doesn't own Minecraft.");
   }
