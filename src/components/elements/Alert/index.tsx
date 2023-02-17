40eaf32c705cf8ecc2193c81fd3b4d554ec24bad
Lucky3028 <Lucky3028@users.noreply.github.com>
2023-02-17 21:13:26 +0900

    feat: add Alert

---
+++ src/components/elements/Alert/index.tsx
@@ -0,0 +1,28 @@
+import {
+  Alert as ChakraAlert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+  AlertProps,
+  Stack,
+} from '@chakra-ui/react';
+
+type Props = {
+  title: string;
+  description?: string;
+  status: NonNullable<AlertProps['status']>;
+};
+
+export const Alert = ({ title, description, status }: Props) => (
+  <ChakraAlert
+    justifyContent="center"
+    flexDirection={['column', 'row']}
+    {...{ status }}
+  >
+    <AlertIcon mb={2} />
+    <Stack direction="column" align="center">
+      <AlertTitle mr={0}>{title}</AlertTitle>
+      {description ? <AlertDescription>{description}</AlertDescription> : null}
+    </Stack>
+  </ChakraAlert>
+);
