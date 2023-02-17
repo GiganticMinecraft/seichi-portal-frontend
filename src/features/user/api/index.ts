dc3feb8ee7726a1bb6c22da0c6d957787e7f947b
Lucky3028 <Lucky3028@users.noreply.github.com>
2023-02-17 14:07:34 +0900

    feat(user): add getMinecraftGameProfile fun

--- src/features/user/api/index.ts
+++ src/features/user/api/index.ts
@@ -1,1 +1,27 @@
-export const getMinecraftGameProfile = async () => {};
+import { getMcProfile } from './getMcProfile';
+import { hasMcAccount } from './hasMcAccount';
+import { requireMcAccessToken } from './requireMcAccessToken';
+import { requireMsAccountAccessToken } from './requireMsAccountAccessToken';
+import { requireXblToken } from './requireXblToken';
+import { requireXstsToken } from './requireXstsToken';
+
+export const getMinecraftGameProfile = async ([
+  instance,
+  account,
+  scopes,
+]: Parameters<typeof requireMsAccountAccessToken>) => {
+  const msAccessToken = await requireMsAccountAccessToken(
+    instance,
+    account,
+    scopes,
+  );
+  const xblToken = await requireXblToken(msAccessToken);
+  const xstsToken = await requireXstsToken(xblToken);
+  const mcAccessToken = await requireMcAccessToken(xstsToken);
+  const hasMc = await hasMcAccount(mcAccessToken);
+  if (!hasMc) {
+    throw new Error('');
+  }
+
+  return getMcProfile(mcAccessToken);
+};
