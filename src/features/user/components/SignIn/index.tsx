0c18e38b90cb78bd8197e6f8b591c7e10d360b14
Lucky3028 <Lucky3028@users.noreply.github.com>
2023-02-17 16:58:36 +0900

    fix: use loginAndGetGameProfile

--- src/features/user/components/SignIn/index.tsx
+++ src/features/user/components/SignIn/index.tsx
@@ -3,17 +3,19 @@ import { useBoolean } from '@chakra-ui/react';
 
 import { Presenter, type PresenterProps } from './presenter';
 
-import { loginRequest } from '../../config/msal';
+import { loginAndGetGameProfile } from '../../api';
 
 type Props = Omit<PresenterProps, 'onClick' | 'isSigningIn'>;
 
 export const SignIn = ({ ...props }: Props) => {
-  const [isSigningIn, { toggle: toggleIsSigningInt }] = useBoolean(false);
+  const [isSigningIn, { on: startSignIn, off: endSignIn }] = useBoolean(false);
   const { instance } = useMsal();
   const onClick = async () => {
-    toggleIsSigningInt();
-    // TODO: catch error
-    await instance.loginPopup(loginRequest).finally(() => toggleIsSigningInt());
+    startSignIn();
+    // TODO: 格納する
+    // TODO: MSアカウントはあるが、MCアカウントがない場合に注意する
+    // NOTE: だからといってすぐサインアウトすると、リロードされてしまう
+    await loginAndGetGameProfile(instance).finally(() => endSignIn());
   };
 
   // eslint-disable-next-line react/jsx-props-no-spreading
