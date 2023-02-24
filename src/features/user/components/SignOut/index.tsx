097f1d0f3ee1176cfe9abd17dc666b44d55fa632
Lucky3028 <Lucky3028@users.noreply.github.com>
2023-02-24 23:37:41 +0900

    feat: remove profile when signout

--- src/features/user/components/SignOut/index.tsx
+++ src/features/user/components/SignOut/index.tsx
@@ -3,21 +3,25 @@ import { useBoolean } from '@chakra-ui/react';
 
 import { Presenter, type PresenterProps } from './presenter';
 
+import { useSetMcProfile } from '../../hooks';
+
 type Props = Omit<PresenterProps, 'onClick' | 'isSigningOut'>;
 
 export const SignOut = ({ ...props }: Props) => {
   const { instance } = useMsal();
   const [isSigningOut, { toggle: toggleIsSigningOut }] = useBoolean(false);
+  const setMcProfile = useSetMcProfile();
   const onClick = async () => {
     toggleIsSigningOut();
     // TODO: catch error
-    // TODO: storeにあるGamerofileをクリアする
     await instance
       .logoutPopup({
         postLogoutRedirectUri: '/signin',
         mainWindowRedirectUri: '/signin',
       })
       .finally(() => toggleIsSigningOut());
+
+    setMcProfile(undefined);
   };
 
   // eslint-disable-next-line react/jsx-props-no-spreading
