e1dfd4b64059e1823d239aca78b532059949e867
Lucky3028 <Lucky3028@users.noreply.github.com>
2023-02-25 23:45:31 +0900

    feat(SignIn): show toast when error

--- src/features/user/components/SignIn/index.tsx
+++ src/features/user/components/SignIn/index.tsx
@@ -1,10 +1,14 @@
 import { useMsal } from '@azure/msal-react';
-import { useBoolean } from '@chakra-ui/react';
+import { useBoolean, useToast, UseToastOptions } from '@chakra-ui/react';
+import { isOk, unwrapErr, unwrapOk } from 'option-t/lib/PlainResult';
+
+import { defaultToastOptions } from '@/libs';
 
 import { Presenter, type PresenterProps } from './presenter';
 
 import { loginAndGetGameProfile } from '../../api';
 import { useSetMcProfile } from '../../hooks';
+import { MsAccountOwnsNoMcAccount } from '../../types';
 
 type Props = Omit<PresenterProps, 'onClick' | 'isSigningIn'>;
 
@@ -12,14 +16,46 @@ export const SignIn = ({ ...props }: Props) => {
   const [isSigningIn, { on: startSignIn, off: endSignIn }] = useBoolean(false);
   const { instance } = useMsal();
   const setMcProfile = useSetMcProfile();
+  const toast = useToast();
   const onClick = async () => {
     startSignIn();
     // TODO: MSアカウントはあるが、MCアカウントがない場合に注意する
     // NOTE: だからといってすぐサインアウトすると、リロードされてしまう
-    const profile = await loginAndGetGameProfile(instance).finally(() =>
+    const profileResult = await loginAndGetGameProfile(instance).finally(() =>
       endSignIn(),
     );
-    setMcProfile(profile);
+
+    if (isOk(profileResult)) {
+      const profile = unwrapOk(profileResult);
+      toast({
+        ...defaultToastOptions,
+        title: 'サインインに成功しました',
+        description: `ようこそ、${profile.name}さん`,
+        status: 'success',
+      });
+      setMcProfile(profile);
+
+      return;
+    }
+
+    const error = unwrapErr(profileResult);
+    const errorToastOptions: UseToastOptions = {
+      ...defaultToastOptions,
+      status: 'error',
+      title: 'サインイン中にエラーが発生しました',
+    };
+
+    if (error instanceof MsAccountOwnsNoMcAccount) {
+      toast({
+        ...errorToastOptions,
+        description: error.message,
+      });
+    } else {
+      toast({
+        ...errorToastOptions,
+        description: `${error.name}(${error.message})`,
+      });
+    }
   };
 
   // eslint-disable-next-line react/jsx-props-no-spreading
