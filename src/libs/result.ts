4af01b3005b2917c3e0d56dfbaf4c3832b6e1125
Lucky3028 <Lucky3028@users.noreply.github.com>
2023-03-05 22:44:45 +0900

    fix(lib): option-tがinternal typeが非公開になったので修正

--- src/libs/result.ts
+++ src/libs/result.ts
@@ -1,5 +1,8 @@
 import { mapErrForResult, mapForResult, Result } from 'option-t/PlainResult';
-import { TransformFn } from 'option-t/internal/Function';
+
+// NOTE: option-tで利用されている内製の型をここで作成
+// https://github.com/option-t/option-t/blob/main/packages/option-t/src/internal/Function.ts#L1
+type TransformFn<in T, out U> = (input: T) => U;
 
 /**
  * Result<T, E>に対して、Ok(T)のときにwhenOk(TransformFn<T, U>)、Err(E)のときにwhenErr(TransformFn<E, F>)の関数をそれぞれ適用したResult<U, F>を返す。
