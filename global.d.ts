0f11234748bb3e1f208ded73ef70c00f7694cb08
Lucky3028 <Lucky3028@users.noreply.github.com>
2023-03-06 15:38:39 +0900

    fix: remove unused env variable

--- global.d.ts
+++ global.d.ts
@@ -4,6 +4,5 @@ declare namespace NodeJS {
   interface ProcessEnv {
     readonly NEXT_PUBLIC_BACKEND_API_URL: string;
     readonly NEXT_PUBLIC_MS_APP_CLIENT_ID: string;
-    readonly NEXT_PUBLIC_MS_APP_REDIRECT_URL: string;
   }
 }
