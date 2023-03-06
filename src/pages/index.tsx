7c1b7c8ab2ef5a1283219ab0931f8bc31cb44930
Lucky3028 <Lucky3028@users.noreply.github.com>
2023-03-06 16:36:57 +0900

    chore: add logging to debug env

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,10 @@
 import { Home } from '@/components/pages/home';
+import { MS_APP_REDIRECT_URL } from '@/const/env';
 
-const Component = () => <Home />;
+const Component = () => {
+  console.log(MS_APP_REDIRECT_URL);
+
+  return <Home />;
+};
 
 export default Component;
