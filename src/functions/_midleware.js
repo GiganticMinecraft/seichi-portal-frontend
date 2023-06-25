dc28a6c150b4091dadf7b87bc1fcd6e5fec9a817
Lucky3028 <Lucky3028@users.noreply.github.com>
2023-06-25 23:00:15 +0900

    feat: add CF Pages functions to add CORS headers

---
+++ src/functions/_midleware.js
@@ -0,0 +1,6 @@
+export const onRequest = async ({ next }) => {
+  const response = await next();
+  response.headers.set('Access-Control-Allow-Origin', 'https://portal.seichi.click');
+  response.headers.set('Access-Control-Max-Age', 60 * 60 * 24);
+  return response;
+};
