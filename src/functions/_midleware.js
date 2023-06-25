a146f7eaaa6c4ac3d1265c2baced501f78bba7c7
Lucky3028 <Lucky3028@users.noreply.github.com>
2023-06-25 23:11:59 +0900

    feat: use ts for cf pages functions

--- src/functions/_midleware.js
+++
@@ -1,9 +0,0 @@
-export const onRequest = async ({ next }) => {
-  const response = await next();
-  response.headers.set(
-    'Access-Control-Allow-Origin',
-    'https://portal.seichi.click',
-  );
-  response.headers.set('Access-Control-Max-Age', 60 * 60 * 24);
-  return response;
-};
