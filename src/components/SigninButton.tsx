843d9c47a2f9a6602a368a180ee1ea05c6b48f64
Lucky3028 <me@mail.cap-l.cc>
2023-10-08 13:21:40 +0900

    refactor: remove debugging codes

--- src/components/SigninButton.tsx
+++ src/components/SigninButton.tsx
@@ -2,27 +2,20 @@
 
 import { useMsal } from '@azure/msal-react';
 import { Button } from '@mui/material';
-import { useEffect, useState } from 'react';
 import { loginRequest } from '@/authConfig';
-import type { AuthenticationResult } from '@azure/msal-browser';
 
 export const SigninButton = () => {
-  const { instance, accounts } = useMsal();
-  const [res, setRes] = useState<AuthenticationResult | undefined>(undefined);
-
-  useEffect(() => console.log(res, accounts), [res, accounts]);
+  const { instance } = useMsal();
 
   const onClick = async () => {
     await instance.initialize();
     await instance
       .acquireTokenSilent(loginRequest)
-      .catch(async () => instance.acquireTokenPopup(loginRequest))
-      .then(setRes);
+      .catch(async () => instance.acquireTokenPopup(loginRequest));
   };
 
   return (
     <Button color="inherit" onClick={onClick}>
-      {res ? res.accessToken : 'なし'}
       サインイン
     </Button>
   );
