f95cd5fd96f401ae515a40f57d06ff8099b2674a
Lucky3028 <me@mail.cap-l.cc>
2023-10-08 16:47:57 +0900

    feat: add acquireMinecraftAccessToken

--- src/api/login.ts
+++ src/api/login.ts
@@ -1,4 +1,9 @@
-import { xboxLiveServiceTokenResponseSchema } from '@/schemas/loginSchema';
+'use server';
+
+import {
+  minecraftAccessTokenResponseSchema,
+  xboxLiveServiceTokenResponseSchema,
+} from '@/schemas/loginSchema';
 
 export const acquireXboxLiveToken = async (token: string) => {
   const URL = 'https://user.auth.xboxlive.com/user/authenticate';
@@ -49,4 +54,27 @@ export const acquireXboxServiceSecurityToken = async ({
   const result = xboxLiveServiceTokenResponseSchema.parse(json);
 
   return { token: result.Token, userHash: result.DisplayClaims.xui[0].uhs };
+};
+
+export const acquireMinecraftAccessToken = async ({
+  token,
+  userHash,
+}: Awaited<ReturnType<typeof acquireXboxServiceSecurityToken>>) => {
+  const URL =
+    'https://api.minecraftservices.com/authentication/login_with_xbox';
+
+  const json = await fetch(URL, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      identityToken: `XBL3.0 x=${userHash};${token}`,
+    }),
+  }).then(async (r) => r.json());
+
+  const result = minecraftAccessTokenResponseSchema.parse(json);
+
+  return { token: result.access_token };
 };
