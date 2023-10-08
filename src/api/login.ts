4c919c40e70ea6bd48dcc3af30d43c7d0d3fa623
Lucky3028 <me@mail.cap-l.cc>
2023-10-08 13:57:27 +0900

    feat: add xbox live service token api

---
+++ src/api/login.ts
@@ -0,0 +1,26 @@
+import { xboxLiveTokenResponseJsonSchema } from '@/schemas/loginSchema';
+
+export const acquireXboxLiveToken = async (token: string) => {
+  const URL = 'https://user.auth.xboxlive.com/user/authenticate';
+
+  const json = await fetch(URL, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      Properties: {
+        AuthMethod: 'RPS',
+        SiteName: 'user.auth.xboxlive.com',
+        RpsTicket: `d=${token}`,
+      },
+      RelyingParty: 'http://auth.xboxlive.com',
+      TokenType: 'JWT',
+    }),
+  }).then(async (r) => r.json());
+
+  const result = xboxLiveTokenResponseJsonSchema.parse(json);
+
+  return { token: result.Token, userHash: result.DisplayClaims.xui[0].uhs };
+};
--- src/components/SigninButton.tsx
+++ src/components/SigninButton.tsx
@@ -2,16 +2,21 @@
 
 import { useMsal } from '@azure/msal-react';
 import { Button } from '@mui/material';
+import { useTransition } from 'react';
+import { acquireXboxLiveToken } from '@/api/login';
 import { loginRequest } from '@/authConfig';
 
 export const SigninButton = () => {
   const { instance } = useMsal();
+  const [_isPending, startTransition] = useTransition();
 
   const onClick = async () => {
     await instance.initialize();
-    await instance
+    const token = await instance
       .acquireTokenSilent(loginRequest)
-      .catch(async () => instance.acquireTokenPopup(loginRequest));
+      .catch(async () => instance.acquireTokenPopup(loginRequest))
+      .then((r) => r.accessToken);
+    startTransition(async () => acquireXboxLiveToken(token).then(console.log));
   };
 
   return (
