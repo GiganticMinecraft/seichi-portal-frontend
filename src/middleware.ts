37973b75119fabe05bc30d23b74406dba153b77d
Lucky3028 <me@mail.cap-l.cc>
2023-10-09 20:23:16 +0900

    feat: 非ログイン時にトップ画面にリダイレクト

---
+++ src/middleware.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from 'next/server';
+import { getCachedToken } from './api/mcToken';
+import type { NextRequest } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  if (request.method !== 'GET') {
+    return;
+  }
+  const pathName = request.nextUrl.pathname.toLowerCase();
+  if (pathName === '/' || pathName.includes('_next/static')) {
+    return;
+  }
+
+  if (!!getCachedToken(request.cookies)) {
+    return;
+  }
+
+  return NextResponse.redirect(new URL('/', request.url));
+}
