a1ff0c97021bb19ddb156351626768c69261cca6
Lucky3028 <me@mail.cap-l.cc>
2023-10-09 20:08:22 +0900

    docs: ログインを迫るメッセージを修正

--- src/components/NeedToSignin.tsx
+++ src/components/NeedToSignin.tsx
@@ -4,7 +4,7 @@ export const NeedToSignin = () => (
   <Box sx={{ width: '100%' }}>
     <Alert severity="error">
       <AlertTitle>サインインしてください</AlertTitle>
-      本サイトをご利用いただくには、MinecraftIDを紐づけたMicrosoftアカウントでのサインインが必要です。
+      本サイトをご利用いただくには、MinecraftIDを紐づけたMicrosoftアカウントでのが必要です。右上のボタンよりサインインしてください。
     </Alert>
   </Box>
 );
