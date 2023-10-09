2f84aab148c5c3a2ab0c84ef733e512fda709fe0
Lucky3028 <me@mail.cap-l.cc>
2023-10-09 20:30:11 +0900

    fix: all backend api requires token

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,11 +1,13 @@
 import { getAllAnswers } from '@/api/form';
+import { getCachedToken } from '@/api/mcToken';
 import DataTable from '@/components/dashboard';
 import DashboardMenu from '@/components/dashboardMenu';
 import NavBar from '@/components/NavBar';
 import styles from '../page.module.css';
 
 export default async function Home() {
-  const answers = await getAllAnswers();
+  const token = getCachedToken() ?? '';
+  const answers = await getAllAnswers(token);
   return (
     <main className={styles['main']}>
       <NavBar />
