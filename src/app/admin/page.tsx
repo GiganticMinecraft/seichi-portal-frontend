30b50472361cc3d2da29a269c75b0a36ad62da50
Lucky3028 <me@mail.cap-l.cc>
2023-11-03 01:40:49 +0900

    fix: use nested layout

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,19 +1,16 @@
 import DataTable from '@/components/Dashboard';
 import DashboardMenu from '@/components/DashboardMenu';
-import NavBar from '@/components/NavBar';
 import { getAllAnswers } from '@/features/form/api/form';
 import { getCachedToken } from '@/features/user/api/mcToken';
-import styles from '../page.module.css';
 
 const Home = async () => {
   const token = getCachedToken() ?? '';
   const answers = await getAllAnswers(token);
   return (
-    <main className={styles['main']}>
-      <NavBar />
+    <>
       <DashboardMenu />
       <DataTable answers={answers} />
-    </main>
+    </>
   );
 };
 
