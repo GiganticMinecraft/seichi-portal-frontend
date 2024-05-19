791d5f317f0674caf217b7b83beba1efe068900b
rito528 <39003544+rito528@users.noreply.github.com>
2024-05-19 18:01:05 +0900

    chore: DEBUG_MODEであるかの比較ができていなかったのを修正

--- src/env.ts
+++ src/env.ts
@@ -1,4 +1,4 @@
 export const MS_APP_CLIENT_ID = process.env.NEXT_PUBLIC_MS_APP_CLIENT_ID;
 export const MS_APP_REDIRECT_URL = process.env.NEXT_PUBLIC_MS_APP_REDIRECT_URL;
 export const BACKEND_SERVER_URL = process.env.BACKEND_SERVER_URL;
-export const DEBUG_MODE = process.env.DEBUG_MODE;
+export const DEBUG_MODE = Boolean(process.env.DEBUG_MODE);
