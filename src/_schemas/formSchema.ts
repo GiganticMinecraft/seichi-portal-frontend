5a32d84d94b819458c1526ce03129b8fa2547af4
rito528 <39003544+rito528@users.noreply.github.com>
2024-05-30 18:02:07 +0900

    refactor: 使用していないスキーマの削除

--- src/_schemas/formSchema.ts
+++ src/_schemas/formSchema.ts
@@ -21,21 +21,6 @@ export const mimimumFormSchema = z.object({
   response_period: responsePeriodSchema,
 });
 
-export const answerSchema = z.object({
-  question_id: z.number(),
-  answer: z.string(),
-});
-
-export const batchAnswerSchema = z.object({
-  uuid: z.string(),
-  timestamp: z.string().datetime(),
-  form_id: z.number(),
-  title: z.string(),
-  answers: z.array(answerSchema),
-});
-
-export type BatchAnswer = z.infer<typeof batchAnswerSchema>;
-
 export type MinimumForm = z.infer<typeof mimimumFormSchema>;
 
 export type FormQuestion = z.infer<typeof questionSchema>;
