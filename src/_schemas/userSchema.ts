51c6f64746a8e079e37325b2d6dc5dd5fb644081
rito528 <39003544+rito528@users.noreply.github.com>
2024-06-28 17:28:45 +0900

    refactor: userSchema.tsを削除

--- src/_schemas/userSchema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-
-const userSchema = z.object({
-  uuid: z.string().uuid(),
-  name: z.string(),
-  role: z.enum(['ADMINISTRATOR', 'STANDARD_USER']),
-});
-
-export type User = z.infer<typeof userSchema>;
