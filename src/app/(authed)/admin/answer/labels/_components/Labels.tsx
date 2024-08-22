44a179ce477c97fd69b7c1ddf41b17551b60aa4d
rito528 <39003544+rito528@users.noreply.github.com>
2024-08-22 17:42:44 +0900

    feat: 回答用ラベル表示部分の実装

--- src/app/(authed)/admin/answer/labels/_components/Labels.tsx
+++ src/app/(authed)/admin/answer/labels/_components/Labels.tsx
@@ -1,3 +1,62 @@
-const Labels = () => {
-  return;
+import { Delete, Edit } from '@mui/icons-material';
+import {
+  Button,
+  List,
+  ListItem,
+  ListItemText,
+  Stack,
+  Typography,
+  Divider,
+} from '@mui/material';
+
+type Label = {
+  id: number;
+  name: string;
 };
+
+const Labels = (props: { labels: Label[] }) => {
+  return (
+    <List sx={{ width: '100%' }}>
+      {props.labels.map((label, index) => (
+        <Stack>
+          <ListItem
+            secondaryAction={
+              <Stack direction="row" spacing={2}>
+                <Button variant="contained" startIcon={<Edit />}>
+                  EDIT
+                </Button>
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  startIcon={<Delete />}
+                >
+                  DELETE
+                </Button>
+              </Stack>
+            }
+            key={index}
+          >
+            <ListItemText sx={{ alignItems: 'center' }} primary={label.name} />
+          </ListItem>
+          <Divider />
+        </Stack>
+      ))}
+    </List>
+  );
+};
+
+const Label = (props: { label: Label }) => {
+  return (
+    <Stack direction="row" spacing={2}>
+      <Typography>{props.label.name}</Typography>
+      <Button variant="contained" startIcon={<Edit />}>
+        EDIT
+      </Button>
+      <Button variant="contained" color="secondary" startIcon={<Delete />}>
+        DELETE
+      </Button>
+    </Stack>
+  );
+};
+
+export default Labels;
--- src/app/(authed)/admin/answer/labels/page.tsx
+++ src/app/(authed)/admin/answer/labels/page.tsx
@@ -2,11 +2,31 @@
 
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import adminDashboardTheme from '../../theme/adminDashboardTheme';
+import useSWR from 'swr';
+import { Either } from 'fp-ts/lib/Either';
+import {
+  ErrorResponse,
+  GetAnswerLabelsResponse,
+} from '@/app/api/_schemas/ResponseSchemas';
+import LoadingCircular from '@/app/_components/LoadingCircular';
+import ErrorModal from '@/app/_components/ErrorModal';
+import Labels from './_components/Labels';
 
 const Home = () => {
+  const { data, isLoading } = useSWR<
+    Either<ErrorResponse, GetAnswerLabelsResponse>
+  >('/api/answers/labels');
+
+  if (!data) {
+    return <LoadingCircular />;
+  } else if ((!isLoading && !data) || data._tag === 'Left') {
+    return <ErrorModal />;
+  }
+
   return (
     <ThemeProvider theme={adminDashboardTheme}>
       <CssBaseline />
+      <Labels labels={data.right} />
     </ThemeProvider>
   );
 };
